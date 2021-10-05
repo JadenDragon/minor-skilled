cfb456a7706704b40581c433bd1632e6826028fb
Jogum Cannegieter <joqum.c@gmail.com>
2021-10-06 01:44:23 +0200

    Created game manager and item controller script. set-up polybuilder package

---
+++ SystemMechanics-Playground/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //references the script to be used
+    public static GameManager current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    //define onItemPickup event
+    public event System.Action onItemPickupTrigger;
+    public void ItemPickup()
+    {
+        if (onItemPickupTrigger != null)
+        {
+            onItemPickupTrigger();
+        }
+    }
+}
---
+++ SystemMechanics-Playground/Assets/Scripts/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calls gameManager and accesses the onItemPickupTrigger action
+        //adds ItemController to gameManer list of subscribed events
+        GameManager.current.onItemPickupTrigger += OnItemPickup;
+    }
+
+
+    //method that listens to event
+    private void OnItemPickup()
+    {
+        //add item to inventory when pickedup
+        //increase item counter
+        Debug.Log("this item is collected" +1);
+    }
+}
