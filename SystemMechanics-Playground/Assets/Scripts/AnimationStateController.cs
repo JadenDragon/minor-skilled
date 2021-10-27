84eae8e1028fec53eed0155bf426a29fbdb090eb
Jogum Cannegieter <joqum.c@gmail.com>
2021-10-28 00:14:36 +0200

    Updated animation states
    learned animation states for freeform movement.
    implemented the code for freeform movement.
    added lateral movement on test animation.

--- SystemMechanics-Playground/Assets/Scripts/AnimationStateController.cs
+++ SystemMechanics-Playground/Assets/Scripts/AnimationStateController.cs
@@ -6,16 +6,26 @@ public class AnimationStateController : MonoBehaviour
 {
 
     Animator animator;
+
     int isWalkingHash;
     int isRunningHash;
+    int VelocityHash;
+
+    //velocity to control animation
+    float anim_Velocity = 0f;
+    public float anim_Acceloration = 0.9f;
+    public float anim_Decceloration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
         //sets isWalking from string to int
+        //increase performance in playMode
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        VelocityHash = Animator.StringToHash("Velocity");
     }
 
     // Update is called once per frame
@@ -37,11 +47,26 @@ public class AnimationStateController : MonoBehaviour
             animator.SetBool(isWalkingHash, false);
 
         //checks if player is moving forward and pressing run
-        if (!isRunning && moveForward && runActive)
+        if (moveForward && runActive && anim_Velocity < 1.0f) 
+        {
+            anim_Velocity += Time.deltaTime * anim_Acceloration;
             animator.SetBool("isRunning", true);
+        }
+            
 
         //stops running if moveForward is not pressed or if not runActive
-        if(isRunning && !moveForward || !runActive)
+        if(!moveForward || !runActive && anim_Velocity > 0.0f)
+        {
             animator.SetBool("isRunning", false);
+            anim_Velocity -= Time.deltaTime * anim_Decceloration;
+        }
+
+        if (!moveForward && anim_Velocity < 0.0f)
+        {
+            //resets the velocity to 0 when it goes below 0
+            anim_Velocity = 0.0f;
+        }
+
+        animator.SetFloat(VelocityHash, anim_Velocity);
     }
 }
